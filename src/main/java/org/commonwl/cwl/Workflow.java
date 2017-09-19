ac7a7777414f2059b61c177d9f6c0606e22a7b57
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-09-19 17:06:31 +0200

    Workflow now inherits from Process.

--- src/main/java/org/commonwl/cwl/Workflow.java
+++ src/main/java/org/commonwl/cwl/Workflow.java
@@ -15,60 +15,22 @@
  */
 package org.commonwl.cwl;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-public class Workflow extends HashMap<String, Object> {
-	private static final long serialVersionUID = -2170400805154307949L;
-	private InputParameter[] inputs;
-	private OutputParameter[] outputs;
-	
-	public static Workflow fromFile(File file) throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());		
-		
-		Workflow wf = mapper.readValue(file, Workflow.class);
-		
-		return wf;
-	}
-	
-	public Workflow(
-			@JsonProperty("inputs") InputParameter[] inputs,
-			@JsonProperty("outputs") OutputParameter[] outputs
-			){
-		super();
-		this.setInputs(inputs);
-		this.setOutputs(outputs);
+public class Workflow extends Process {
+	private HashMap<String, Process> steps;
+
+	public Workflow(String id){
+		super(id);
+		steps = new HashMap<>();
 	}
 
-	public InputParameter[] getInputs() {
-		return inputs;
+	public void addStep(String id, Process step) {
+		steps.put(id, step);
 	}
 
-	public void setInputs(InputParameter[] inputs) {
-		this.inputs = inputs;
-	}
-
-	public OutputParameter[] getOutputs() {
-		return outputs;
-	}
-
-	public void setOutputs(OutputParameter[] outputs) {
-		this.outputs = outputs;
-	}
-
-	public static Workflow fromInputStream(InputStream inputStream) throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());		
-		
-		Workflow wf = mapper.readValue(inputStream, Workflow.class);
-		
-		return wf;
+	public HashMap<String, Process> getSteps() {
+		return steps;
 	}
 }
